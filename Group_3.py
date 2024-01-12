4231c6d4f3432d7917f76eb8fe5d093d527cd728
Habiba-Hussein <147278092+Habiba-Hussein@users.noreply.github.com>
2024-01-12 23:19:42 +0300

    I imported the request library and the welcome message

---
+++ Group_3.py
@@ -0,0 +1,7 @@
+from pip._vendor import requests
+ 
+welcome_msg = """Welcome to the Global Pandemic (COVID-19) Tracker!
+Your one-stop app for information about COVID-19 around the globe.
+To stay up-to-date with relevant information about the pandemic in different countries around the globe,
+this app is here to meet your needs."""
+ 
