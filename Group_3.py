ca5fecac999a00e036847530c88b61ff50767a28
Nwachukwu Esther <kaluesther101@gmail.com>
2024-01-12 22:05:19 +0100

    i wrote the display country info function

--- Group_3.py
+++ Group_3.py
@@ -4,4 +4,13 @@ welcome_msg = """Welcome to the Global Pandemic (COVID-19) Tracker!
 Your one-stop app for information about COVID-19 around the globe.
 To stay up-to-date with relevant information about the pandemic in different countries around the globe,
 this app is here to meet your needs."""
+
+def display_country_info(country_info):
+    continent = ', '.join(country_info[0]['continents'])
+    area_value = country_info[0]['area']
+    population = country_info[0]['population']
+    capital = ', '.join(country_info[0]['capital'])
+    timezone = ', '.join(country_info[0]['timezones'])
+    languages = ', '.join(f"{k}: {v}" for k, v in country_info[0]['languages'].items())
+    currencies = ', '.join(f"{k}: {v['name']} ({v['symbol']})" for k, v in country_info[0]['currencies'].items())
  
