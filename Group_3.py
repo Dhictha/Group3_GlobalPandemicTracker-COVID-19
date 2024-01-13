675fb7318f8d7e9a3adadb465592afa02dd9925b
unknown <leishafa@gmail.com>
2024-01-13 12:02:45 +0300

    Indentation

--- Group_3.py
+++ Group_3.py
@@ -19,13 +19,14 @@ def display_country_info(country_info):
     languages = ', '.join(f"{k}: {v}" for k, v in country_info[0]['languages'].items())
     currencies = ', '.join(f"{k}: {v['name']} ({v['symbol']})" for k, v in country_info[0]['currencies'].items())
 
-msg = (f"{country} has a total population of {population} people.\n"
+    msg = (f"{country} has a total population of {population} people.\n"
            f"Out of the {population} people, {tested_positive} tested positive for COVID-19 and {tested_negative} tested negative.\n"
            f"Fortunately, some people survived COVID-19, but the number of people who couldn't survive was {deaths}.\n"
            f"That's very unfortunate! \nPopulation left after COVID: {population_left_after_covid}")
  
-with open("covid_data.txt", "w") as txt_file:
+    with open("covid_data.txt", "w") as txt_file:
         txt_file.write(msg + '\n') 
+        
 def display_covid_info(covid_info):
     cases = covid_info['cases']
     deaths = covid_info['deaths']
